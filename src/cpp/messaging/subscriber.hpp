19890ad782c953dc9fe449897e4d25df1c7c5389
Lmauser747 <lucasm2818@gmail.com>
2026-09-13 11:37:35 -0700

    Added spin once to subscriber.hpp

--- src/cpp/messaging/subscriber.hpp
+++ src/cpp/messaging/subscriber.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include <zmq.h>
 #include <zmq.hpp>
 #include <string>
-#include <google/protobuf/message.h>
+#include <google/protobuf/message_lite.h>
+#include <chrono>
+
 
 template<typename MessageType>
 class Subscriber{
@@ -17,10 +20,10 @@ class Subscriber{
      
 
 public:
-    Subscriber(const std::string &addressStr, const std::string &topicStr, CallbackType callbackFn, bool bind = false) :
-    address(std::move(addressStr)), topic(std::move(topicStr)), callback(std::move(callbackFn)){
+    Subscriber(const std::string addressStr, const std::string topicStr, CallbackType callbackFn, bool bind = false) :
+    address(std::move(addressStr)), topic(std::move(topicStr)), callback(callbackFn){
         context = zmq::context_t(1);
-        socket = zmq::socket_t(context, zmq::socket_type::pub);
+        socket = zmq::socket_t(context, zmq::socket_type::sub);
         if(bind){
             socket.bind(address);
         }else{
@@ -29,5 +32,43 @@ public:
 
         socket.set(zmq::sockopt::subscribe, topic);
 
+    }
+
+    bool spinOnce(std::chrono::milliseconds timeout_ms){
+        zmq::pollitem_t items[] = {
+            { static_cast<void*>(socket), 0, ZMQ_POLLIN, 0 }
+        };
+
+        zmq::poll(&items[0], 1, std::chrono::milliseconds(timeout_ms));
+
+        if (items[0].revents & ZMQ_POLLIN) {
+            try {
+                zmq::message_t topicMsg;
+                zmq::recv_result_t topicResult = socket.recv(topicMsg, zmq::recv_flags::none);
+
+                if (!topicResult || !topicMsg.more()) {
+                    return false;
+                }
+
+                zmq::message_t payloadMsg;
+                zmq::recv_result_t payloadResult = socket.recv(payloadMsg, zmq::recv_flags::none);
+
+                if (!payloadResult) {
+                    return false;
+                }
+
+                MessageType message;
+                if (message.ParseFromArray(payloadMsg.data(), payloadMsg.size())) {
+                    callback(message);
+                    return true;
+                } else {
+                    std::cerr << "Error parsing message on topic '" << topic << "'\n";
+                }
+            } catch (const std::exception& e) {
+                std::cerr << "Error parsing message on topic '" << topic << "': " << e.what() << "\n";
+            }
+            
+        }
+        return false;
     }
 };
